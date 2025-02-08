8a93a54ce23df8e07ce55a1fca38097c4b41e6c1
Davidnna <davedozie1@gmail.com>
2025-02-08 02:29:02 +0100

    push

---
+++ script.js
@@ -0,0 +1,11 @@
+window.onload = function() {
+    const card = document.getElementById("card");
+
+    setTimeout(() => {
+        card.style.backgroundColor = "yellow";
+    }, 3000);
+    
+    setTimeout(() => {
+        card.style.backgroundColor = "#9067B5";
+    }, 7000);
+};
